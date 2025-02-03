cf5d2bccb0a00f75cf99f1efc8d93d13fb805086
Joel Ajay <ajaijoel@gmail.com>
2025-02-03 18:41:43 +0530

    fix: ratelimit delay for the API fetcher

--- data-collection/index.js
+++ data-collection/index.js
@@ -63,14 +63,14 @@ const processRequest = async (symbolToken, from, to) => {
         fromdate: from,
         todate: to
     });
-    console.log("request status: " + response.status)
-    if(response.status === true){
+    if(response.status === true && response.data.length !== 0){
         appendJsonToFile('data.json', response.data);
-        console.log(`Saved data from ${formatDate(currentStart)} to ${formatDate(currentEnd)}`);     
+        console.log(`Saved data from ${from} to ${to}`);     
     }
     else{
-        console.log(response);
-        throw Error("error");
+        
+        console.log('Failed response');
+        console.log(response)
     }
 }
 const loginNow = async () => {
@@ -80,23 +80,27 @@ const loginNow = async () => {
 }
 
 const getRequests = async () => {
-    try {      
-
-
+    try {
         const startDate = new Date('2014-02-10T09:00:00');
         const endDate = new Date('2024-02-10T09:00:00');
         const maxDaysPerRequest = 30;
 
         let currentStart = new Date(startDate);
-        
+        let i = 0;
         while (currentStart < endDate) {
+
+            //5 second delay to prevent getting ratelimited
+            await sleep(5000);
+
+            //Refresh login and TOTP 
+            if(i %3 == 0) await loginNow();
+            i++;
+
             const currentEnd = new Date(Math.min(currentStart.getTime() + maxDaysPerRequest * 24 * 60 * 60 * 1000, endDate.getTime()));
             
             processRequest("14366", formatDate(currentStart), formatDate(currentEnd));
             
             currentStart = new Date(currentEnd.getTime() + 60000); // Add 1 minute to avoid overlap
-            sleep(100);
-            
         } 
     }   
     catch (err) {
@@ -104,6 +108,11 @@ const getRequests = async () => {
     }
 }
 
-loginNow();
-processRequest("14366", "2014-04-11 03:32", "2014-05-11 03:32")
-// getRequests();
+const main = async () => {
+
+    await loginNow();
+    //await processRequest("14366", "2014-04-11 03:32", "2014-05-11 03:32")
+    await getRequests();
+}
+
+main();
